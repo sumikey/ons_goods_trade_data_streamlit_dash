278bc98fe08998d8c4fa9bcf7bf07231a32f010c
Michael Sullivan <86547947+sumikey@users.noreply.github.com>
2022-04-26 23:09:35 +0800

    Add files via upload
    Uploading new dashboard_app.py and ons_data_collection.py files that work with the test data set.

---
+++ dashboard_app.py
@@ -0,0 +1,245 @@
+import pandas as pd
+import streamlit as st
+import ons_data_collection
+import pickle
+
+# # import our ONS data which is already saved in dataframe pkl file
+# try: 
+#     df = 
+# except:
+#     ons_data_collection.get_all_data()
+#     df = pd.read_pickle('./Data/Dataframe/ons_df.pkl')
+
+# # set a start parameter with an experimental memo cacher
+# @st.experimental_memo
+# def start():
+#     ons_data_collection.get_all_data()
+#     df = pd.read_pickle('ons_df.pkl')
+#     return df
+
+def get_test_data():
+    # read in csv file
+    df = pd.read_csv('ons_csv_test.csv')
+    # fix the columns
+    # add MultiIndex column headers and convert to time-series with datetime
+    df = ons_data_collection.fix_df_columns(df)
+    df = ons_data_collection.df_to_MultiIndex_time_series(df)
+    return df
+
+# start it
+df = get_text_data()
+    
+# get our commodity list from pkl file
+open_file=open('./Data/pkl_lists/commodity_list.pkl', 'rb')
+commodity_list = pickle.load(open_file)
+open_file.close()
+
+# get our partner list from pkl file
+open_file=open('./Data/pkl_lists/partner_list.pkl', 'rb')
+partner_list = pickle.load(open_file)
+open_file.close()
+
+st.write("""
+# UK Goods Exports  
+
+#### Data: [ONS: Trade in goods: country-by-commodity exports](https://www.ons.gov.uk/economy/nationalaccounts/balanceofpayments/datasets/uktradecountrybycommodityexports)
+The data underlying this dashboard is pulled directly from the ONS's website, it may take a while to load upon first opening the website. 
+
+This dashboard is for analysing UK goods exports to different trading partners around the world. The first section looks at the UK's total goods exports to the selected trade partner. The second section looks at UK exports to that same partner by SITC 1 digit product categories, products to be included can be turned on/off via the multi-selector. The third section compares UK exports of the selected partner and product to a range of other trading partners. By default all charts are on a monthly basis but rolling monthly values can be set using the slides in the page's sidebar. Minimum and maximum date ranges for the charts can be set using the sliders in the sidebar. Side bar sliders controll all the visuals at once.
+
+---
+
+""")
+
+## SETTING UP THE FIRST THREE PLOTS FOR TOTAL EXPORTS
+
+# setting up select boxes for our list of partners and trade products
+partner_select = st.selectbox('Which main trade partner do you want to analyse?', partner_list)
+
+# use these values to filter df for first two charts
+plot_df = (df.xs(partner_select, axis=1, level=0)
+             .xs('Total', axis=1, level=1)
+          )
+# rename the columns to the partner name
+plot_df.columns = [partner_select]
+
+# setting up a slider to choose rolling level
+rol_val = st.sidebar.slider('Monthly Rolling Sum (set as "1" for no rolling)', min_value=1, max_value=12)
+plot_df = plot_df.rolling(rol_val).sum().dropna() # edit the plotting df accordingly
+
+# setting a yearly range to be diplayed on the axis
+# set as two separate slides for min and max
+min_year = st.sidebar.slider('Date Range - Minimum', 
+                               min_value=plot_df.index.min().year, 
+                               max_value=plot_df.index.max().year
+                              )
+
+max_year = st.sidebar.slider('Date Range - Maximum', 
+                               min_value=plot_df.index.min().year, 
+                               max_value=plot_df.index.max().year,
+                              value=2022
+                              )
+# convert the min and max years to strings so can use with .loc to index
+min_year = str(min_year)
+max_year = str(max_year)
+
+# write a title for the first section
+st.write(f"""
+---
+##### Total UK Exports to {partner_select}  
+This section shows the UK's total exports to the selected partner: the total value, the yoy change in £s and the yoy change in % terms. Values can be set to rolling monthly sums using the slider in the page's sidebar.""")
+
+# create plotting dfs that are filtered by year range
+plot_abs_df = plot_df.round(1).loc[min_year:max_year] # our plot for absolute values
+plot_diff_df = plot_df.diff(12).round(1).loc[min_year:max_year] # our plot for absolute values
+plot_percent_df = plot_df.pct_change(12).mul(100).dropna().round(1).loc[min_year:max_year] # our plot for yoy change
+
+# create three separate columns
+cola1, cola2, cola3 = st.columns((1,1,1))
+
+with cola1: 
+    # plot a line chart of monthly absolute values
+    if rol_val > 1:
+        st.write(f"UK total exports to {partner_select}, rolling {str(rol_val)}M sum") #title
+    else:
+        st.write(f"UK total exports to {partner_select}, monthly")
+    st.line_chart(plot_abs_df)           #line chart
+
+with cola2: 
+    # plot a line chart of gbp yoy change
+    if rol_val > 1:
+        st.write(f"UK exports to {partner_select} by SITC 1 digit, rolling {str(rol_val)}M sum, GBP change yoy") #title
+    else:
+        st.write(f"UK exports to {partner_select} by SITC 1 digit, monthly, GBP change yoy")
+    st.line_chart(plot_diff_df)           #line chart
+
+with cola3:
+    # plot a line of monthly yoy % change
+    if rol_val > 1:
+        st.write(f"UK total exports to {partner_select}, rolling {str(rol_val)}M sum, % change yoy") #title
+    else:
+        st.write(f"UK total exports to {partner_select}, monthly, % change yoy")
+    st.line_chart(plot_percent_df)
+
+### CREATING SECOND ROW OF THREE PLOTS
+    
+# create a list of codes for the SITC one-digit categories
+sitc1_list = ['1','2','3','4','5','6','7','8','9']
+# index our df using the new sitc list and our selected partner
+# Transpose the df, use pd index slice and transpose back
+idx = pd.IndexSlice
+plot_sitc1_df = df.T.loc[idx[partner_select,sitc1_list,:,:] ,:].T
+# create a list of names from the comm_desc part of our multi-index
+# set this list of names as the column names
+sitc_1dig_names = [x[2] for x in plot_sitc1_df.columns]
+plot_sitc1_df.columns = sitc_1dig_names
+
+# add the rolling factor
+plot_sitc1_df2 = plot_sitc1_df.rolling(rol_val).sum().dropna() # edit the plotting df accordingly
+
+# create three new plot dfs using this newly subsetted dataframe
+plot_sitc1_abs_df = plot_sitc1_df2.round(1).loc[min_year:max_year] # for plotting absolute values
+plot_sitc1_diff_df = plot_sitc1_df2.diff(12).round(1).loc[min_year:max_year] # for plotting absolute values
+plot_sitc1_percent_df = plot_sitc1_df2.pct_change(12).mul(100).dropna().round(1).loc[min_year:max_year] # the % change yoy
+
+st.write(f"""
+
+---
+
+##### UK Exports to {partner_select} by SITC 1 Digit Code
+
+This section shows the UK's exports to the selected partner by SITC 1 digit: the total value, the yoy change in £s and the yoy change in % terms. SITC 1 digit categories can be turned on/off using the multi-selector. Values can be set to rolling monthly sums using the slider in the page's sidebar."""
+        )
+
+# setting up the multi-selector
+sitc1_abs_list = st.multiselect('Select SITC 1 digit to include', sitc_1dig_names, default=sitc_1dig_names)
+
+# create three separate columns
+colb1, colb2, colb3 = st.columns((1,1,1))
+
+with colb1: 
+    # plot a bar chart of monthly absolute values
+    if rol_val > 1:
+        st.write(f"UK exports to {partner_select} by SITC 1 Digit, rolling {str(rol_val)}M sum") #title
+    else:
+        st.write(f"UK exports to {partner_select} by SITC 1 Digit, monthly")   
+    st.bar_chart(plot_sitc1_abs_df[sitc1_abs_list])#[sitc1_abs_list])           #line chart
+
+with colb2: 
+    # plot a bar chart of monthly absolute values
+    if rol_val > 1:
+        st.write(f"UK exports to {partner_select} by SITC 1 Digit, rolling {str(rol_val)}M sum, GBP yoy change") #title
+    else:
+        st.write(f"UK exports to {partner_select} by SITC 1 Digit, monthly, GBP yoy change")
+    st.bar_chart(plot_sitc1_diff_df[sitc1_abs_list])           #line chart
+
+with colb3: 
+    # plot a bar chart of monthly absolute values
+    if rol_val > 1:
+        st.write(f"UK exports to {partner_select} by SITC 1 Digit, rolling {str(rol_val)}M sum, % yoy change") #title
+    else:
+        st.write(f"UK exports to {partner_select} by SITC 1 Digit, monthly, % yoy change")
+    st.line_chart(plot_sitc1_percent_df[sitc1_abs_list])           #line chart
+
+### CREATING THE THIRD ROW OF PLOTS
+
+st.write(f"""
+---  
+##### UK exports to selected partners, for selected product category
+
+This section shows the UK's exports of the selected product categoory to the range of selected partners by the total value, the yoy change in £s and the yoy change in % terms. The product category and comparison partners can be set using the options. Values can be set to rolling monthly sums using the slider in the page's sidebar."""
+        )
+
+# setup a list of starting comparitors
+comparitors = [partner_select, 'Whole world', 'Total EU(28)', 'Extra EU 28 (Rest of World)']
+
+product_select = st.selectbox('Select a product to compare across partners', commodity_list)
+
+# setting up the trading partner multi-selector
+multipartner_select = st.multiselect('Select trade partners to include', partner_list, default=comparitors)
+
+# index our dataframe using our selections
+idx = pd.IndexSlice
+plot_compare_df = df.T.loc[idx[multipartner_select,:,product_select,:] ,:].T
+
+# create a list of names from the comm_desc part of our multi-index
+# set this list of names as the column names
+compare_names = [x[0] for x in plot_compare_df.columns]
+plot_compare_df.columns = compare_names
+
+# add the rolling factor
+plot_compare_df = plot_compare_df.rolling(rol_val).sum().dropna() # edit the plotting df accordingly
+
+# create three new plot dfs using this newly subsetted dataframe
+plot_compare_abs_df = plot_compare_df.round(1).loc[min_year:max_year] # for plotting absolute values
+plot_compare_diff_df = plot_compare_df.diff(12).dropna().round(1).loc[min_year:max_year] # for plotting absolute values
+plot_compare_percent_df = plot_compare_df.pct_change(12).mul(100).dropna().round(1).loc[min_year:max_year] # the % change yoy
+
+# create three separate columns
+colc1, colc2, colc3 = st.columns((1,1,1))
+
+with colc1: 
+    # plot a line chart of monthly absolute values
+    if rol_val > 1:
+        st.write(f"UK {product_select} exports to {partner_select}, rolling {str(rol_val)}M sum") #title
+    else:
+        st.write(f"UK {product_select} exports to {partner_select}, monthly")
+    st.line_chart(plot_compare_abs_df)           #line chart
+
+with colc2: 
+    # plot a line chart of gbp yoy change
+    if rol_val > 1:
+        st.write(f"UK {product_select} exports to {partner_select}, rolling {str(rol_val)}M sum, GBP change yoy") #title
+    else:
+        st.write(f"UK {product_select} exports to {partner_select}, GBP change yoy")
+    st.line_chart(plot_compare_diff_df)           #line chart
+
+with colc3:
+    # plot a line of monthly yoy % change
+    if rol_val > 1:
+        st.write(f"UK {product_select} exports to {partner_select}, rolling {str(rol_val)}M sum, % change yoy") #title
+    else:
+        st.write(f"UK {product_select} exports to {partner_select}, monthly, % change yoy")
+    st.line_chart(plot_compare_percent_df)
+
+
---
+++ ons_data_collection.py
@@ -0,0 +1,220 @@
+ # imports
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from zipfile import ZipFile
+from io import BytesIO
+import glob
+import streamlit as st
+
+def get_latest_onscbcex_zip_url():
+    """A function to return a URL for the latest version of the ONS's Country by Commodity Exports data.
+    Scrapes the relevant ONS webpage and returns the latest/current version."""
+    
+    # print output to show where in process
+    print('Getting URL for latest ONS data')
+    
+    # ONS CBC url to scrape
+    url = 'https://www.ons.gov.uk/economy/nationalaccounts/balanceofpayments/datasets/uktradecountrybycommodityexports/current'
+    
+    # download webpage
+    # parse html
+    doc = requests.get(url)
+    soup = BeautifulSoup(doc.text, features="lxml")
+    
+    # find all the h3 elements as a list
+    # take the index of the element whose text is latest version
+    for index, entry in enumerate(soup.find_all('h3')):
+        if entry.text == 'Latest version':
+            i = index
+
+    # construct a url from base plus the href
+    # look in parent and then a within that parent
+    file_url = 'https://www.ons.gov.uk' + soup.find_all('h3')[i].parent.a['href']
+    
+    return file_url
+
+def get_historic_onscbcex_zip_url():
+    """A function to return a url for the historic (1997-2017) version of ONS's Country by Commodity Exports data.
+    Scrapes the relevant ONS webpage and returns the current historic version"""
+    
+    # print output to show where in process
+    print('Getting URL for historic ONS data')
+    
+    # ONS source page url to scrape
+    url = 'https://www.ons.gov.uk/economy/nationalaccounts/balanceofpayments/datasets/uktradecountrybycommodityexports'
+    
+    # download webpage
+    # parse html
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, features="lxml")
+    
+    # create file url add_on based on html
+    # add onto the base url
+    file_url = soup.find_all('li', {'class':"margin-top--0 margin-bottom--0"})[0].a['href']
+    new_url = 'https://www.ons.gov.uk' + file_url
+    
+    return new_url
+
+def download_unzip_onscbcex(url, data_folder='Current'):
+    """A function to download a latest copy of the ONS's Country by Commodity export data.
+    Uses our get_latest_onscbcex_zip_url() function to get url, downloads with requests.
+    Uses Zipfile and Bytes IO to unzip and save in folder /Data. """
+    
+    # print output to show where in process
+    print('Beginning download and unzip of data files')
+        
+#     # Split URL to get the file name
+#     filename = url.split('/')[-1]
+    
+    # Downloading the file by sending the request to the URL
+    req = requests.get(url)
+    print(f'Downloading {data_folder} Data Completed')
+    
+    # extracting the zip file contents
+    zipped= ZipFile(BytesIO(req.content))
+    zipped.extractall(f'Data/{data_folder}/')
+    print(f'{data_folder} data extracted from zip and saved in Data/{data_folder} folder.')
+    
+    # empty return
+    return
+
+def create_single_dataframe():
+    """Finds the filepaths of the recently downloaded current and historic excel files.
+    Extracts relevant parts from excel files, and converts to dataframes.
+    Merges the historic and current datasets, and returns merged df."""
+    
+    # print output to show where in process
+    print('Beginning creation of single dataframe')
+    
+    # create filepath names using glob
+    filepath_h = glob.glob('Data/Historic/'+'*.xlsx')[0]
+    filepath_c = glob.glob('Data/Current/'+'*.xlsx')[0]
+    
+    # read in dataframes using pandas
+    print('Converting excel files to pandas dataframes, may take a while')
+    df_c = pd.read_excel(filepath_c,sheet_name='3. Monthly Exports', skiprows=3)
+    df_h = pd.read_excel(filepath_h, sheet_name='1. Country by Commodity Export', skiprows=3)
+    print('Dataframe converstion completed, merging')
+    
+    # merge the two dataframes, add current columns after historic
+    df = pd.merge(df_h, df_c, on=['COMMODITY', 'COUNTRY','DIRECTION'])
+    
+    print('Completed')
+    # return merged df
+    return df
+
+def fix_df_columns(df):
+    """A function to fix the dataframe columns included by default on ONS Country by Commodity Exports dataset.
+    Removes the country codes from inside the country names column, splits the commodity column into code and description,
+    removes the codes from the direction column"""
+    
+    # print output to show where in process
+    print('Preprocessing dataframe columns')
+    
+    # create a list of country names only with codes removed
+    country_names_only = []
+    for item in df.COUNTRY:
+        country_names_only.append(item[3:])
+    
+    # create a list of flow names only with codes removed
+    flow_names_only = []
+    for item in df.DIRECTION:
+        flow_names_only.append(item[3:])
+
+    # split the commodity column into the code and description lists
+    comm_codes = []
+    comm_desc = []
+    for item in df.COMMODITY:
+        for i in range(1,6):
+            if item[i] == ' ':
+                comm_codes.append(item[:i])
+                comm_desc.append(item[i:].strip())
+    
+    # use our lists to overwrite three old columns and add new one COMM_CODE
+    df['COUNTRY'] = country_names_only
+    df['COMMODITY'] = comm_desc
+    df['COMM_CODE'] = comm_codes
+    df['DIRECTION'] = flow_names_only
+    
+    # return the adjusted df
+    return df
+
+def df_to_MultiIndex_time_series(df):
+    """Adds a list of multi-index column """
+    
+    # print output to show where in process
+    print('Adding Multi-Index Columns')
+    
+    # create empty list
+    list_tuples = []
+    # create a list of tuples from the zipped columns
+    for item in zip(df.COUNTRY,df.COMM_CODE, df.COMMODITY,df.DIRECTION):
+        list_tuples.append( (item[0], item[1], item[2], item[3]) )
+    # convert to multi_index object
+    index_tuples = pd.MultiIndex.from_tuples(list_tuples)
+    
+    # remove our categorical columns which are now in Multi-Index
+    df = df.iloc[:,3:].iloc[:,:-1]
+    
+    # attach our MultiIndex to dataframe
+    df.index = index_tuples
+    
+    # transpose to have MultIndex columns and arranged as time series
+    df=df.T
+    
+    # convert date strings into datetime-able format
+    # perform datetime conversion
+    df.index = [date[0:4]+'-'+date[4:] for date in df.index]
+    df.index = pd.to_datetime(df.index)
+    
+    # return the altered index
+    return df
+
+def get_all_data():
+    """Creates a dataframe with a multi-index header of UK exports to all countries.
+    Based on ONS Country by commodity exports. Takes historic data and current data and combines.
+    Outputs a single dataframe """
+    
+    # print output to show started
+    print('Beginning process to get all data')
+    
+    # scrape relevant zips from ONS website
+    hist_url = get_historic_onscbcex_zip_url()
+    current_url = get_latest_onscbcex_zip_url()
+    
+    # download and unzip files, save excel files into folders
+    download_unzip_onscbcex(hist_url, data_folder='Historic')
+    download_unzip_onscbcex(current_url, data_folder='Current')
+    
+    # create a single dataframe from the historic and current excel files
+    df = create_single_dataframe()
+    
+    # fix the columns
+    # add MultiIndex column headers and convert to time-series with datetime
+    df = fix_df_columns(df)
+    df = df_to_MultiIndex_time_series(df)
+    
+    # save df as a pick object so maintain multi-headers
+    df.to_pickle('./Data/Dataframe/ons_df.pkl')
+    
+    # print output to say completed
+    print('Completed process to get all data')
+      
+    # empty return
+    return
+
+def get_test_data():
+    
+    # read in csv file
+    df = pd.read_csv('ons_csv_test.csv')
+    
+    # fix the columns
+    # add MultiIndex column headers and convert to time-series with datetime
+    df = fix_df_columns(df)
+    df = df_to_MultiIndex_time_series(df)
+    
+    return df
+    
+    
+    
